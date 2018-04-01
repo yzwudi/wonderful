cd8b05c482af6dbdd1f96233f6963311413192c1
yzwudi <657774676@qq.com>
2018-04-01 21:51:34 +0800

    hh

--- application/fund/admin/fetch.php
+++ application/fund/admin/fetch.php
@@ -9,14 +9,23 @@
 namespace app\fund\admin;
 
 use app\common\controller\Common;
+use think\Request;
 
 /**
  * cms 后台模块
  */
 class Fetch extends Common
 {
+    public function __construct(Request $request = null)
+    {
+        parent::__construct($request);
+        if (!IS_CLI) {
+             exit;
+        }
+    }
+
     public function index()
     {
-        var_dump(IS_CLI) ;
+        echo 'success';
     }
 }
