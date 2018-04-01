7674692b5df96b117955f53a79d86cf492eb1faf
Administrator <171143921@qq.com>
2018-04-01 12:26:46 +0800

    test.php

--- application/function.php
+++ application/function.php
@@ -11,3 +11,17 @@
 
 // 为方便系统核心升级，二次开发中需要用到的公共函数请写在这个文件，不要去修改common.php文件
 
+function jdump($arr)
+{
+    echo json_encode($arr, JSON_UNESCAPED_UNICODE);
+}
+
+function croot3($num)
+{
+    $guess=$num/3;
+    while(abs($guess*$guess*$guess-$num)>=0.0000000001)
+    {
+        $guess=($num/$guess/$guess+2*$guess)/3;
+    }
+    return $guess;
+}
