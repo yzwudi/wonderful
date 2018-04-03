7c48df28286bc255d20ca7a1e6d3d9914eed2ffb
yzwudi <657774676@qq.com>
2018-04-03 23:08:47 +0800

    fetch

--- application/function.php
+++ application/function.php
@@ -26,3 +26,25 @@ function croot3($num)
     }
     return $guess;
 }
+
+function curl($url)
+{
+    $ch = curl_init();
+    // 2. 设置选项，包括URL
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+    curl_setopt($ch,CURLOPT_HEADER,0);
+    // 3. 执行并获取HTML文档内容
+    $output = curl_exec($ch);
+    if($output === FALSE ){
+        echo "CURL Error:".curl_error($ch);
+    }
+    // 4. 释放curl句柄
+    curl_close($ch);
+    return $output;
+}
+
+function getMillisecond() {
+    list($t1, $t2) = explode(' ', microtime());
+    return (float)sprintf('%.0f',(floatval($t1)+floatval($t2))*1000);
+}
--- application/fund/admin/fetch.php
+++ application/fund/admin/fetch.php
@@ -16,16 +16,38 @@ use think\Request;
  */
 class Fetch extends Common
 {
+    const HS300 = "0003001";
+    const CYBZ = "3990062";
+
+    public $index_map = [
+        self::HS300 => '沪深300',
+        self::CYBZ => '创业板指',
+    ];
+
     public function __construct(Request $request = null)
     {
         parent::__construct($request);
-        if (!IS_CLI) {
+        if (!IS_CLI and false) {
              exit;
         }
     }
 
     public function index()
     {
-        echo 'success';
+        $url_tpl = 'http://pdfm2.eastmoney.com/EM_UBG_PDTI_Fast/api/js?id=%s&TYPE=k&js=fsData%s((x))&rtntype=5&isCR=false&fsData%s=fsData%s';
+        foreach ($this->index_map as $code => $name) {
+            $micro_time = getMillisecond();
+            $url = sprintf($url_tpl, $code, $micro_time, $micro_time, $micro_time);
+            $return = curl($url);
+            $return = $this->formatCurlReturn($return);
+        }
+    }
+
+    private function formatCurlReturn($data)
+    {
+        $data = preg_replace("/[a-zA-Z0-9]*[\(\)]/", '', $data);
+        $data = json_decode($data, true);
+        jdump($data);exit;
+        echo $data;exit;
     }
 }
